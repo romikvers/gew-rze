fff6b5c6b8c804d7ac6a1d2fb2f1d25d995c26e9
Roman Koshiv <koshivr032@gmail.com>
2021-05-30 22:24:54 +0300

    Hallo meine freunde

---
+++ lab_№13.2.1.cpp
@@ -0,0 +1,94 @@
+﻿/*
+ lab_13_2.
+ Обчислити площу круга, знайти максимум з двох чисел, використовуючи
+ макроси піднесення до квадрата, виведення результатів, знаходження
+ максимального значення. За допомогою директив умовної компіляції
+ активізувати різні частини програми.
+*/
+#pragma warning(disable : 4996) // функція scanf застаріла, тому пропускаю попеередження
+#include <math.h>
+#include <stdio.h>
+#include <conio.h>
+
+#define z 1 // директива препроцесора – макровизначення
+#define MAX(x, z) (x, z)
+#define MIN(x, z) (x + z, x * z)
+#define PLUS(value) (fabs(value))
+#define SQR(value) (value)*(value)   // макрос з параметром – визначення квадрата числа
+#define MULTI(value, k) (pow(value, k))   // макрос визначення найбільшого з двох чисел
+#define MOREMAX(a_kvadr, value) ((a_kvadr)>(value))?(a_kvadr):(value) // макрос як умовний вираз
+
+#define SECOND(x, z) pow(pow(abs(x - z), z), 2) // виводимо значення поточного виразу
+#define PRINTI(w) puts("control output"); \
+ printf(#w"=%d\n",w)
+ // використовується символ продовження макроса '\'
+ // символ '#' використовується для об’єднання рядків
+ // виведення повідомлення та значення дійсного числа
+#define PRINTR(w) puts ("rezult :"); \
+ printf (#w"=%f",(float)w)
+
+void main()
+{
+	puts("Lab 13.2. Using macros and preprocessing directive");
+	int x, y, k, // змінні для розрахунків
+		max, min,
+		modul, // модуль
+		a_kvadr, // квадрат числа
+		value, stepin,
+		maxim; // максимальне з трьох чисел
+	char ch; // відповідь чи продовжувати виконання програми
+	do
+	{
+#if (5 < z && z < 10) // якщо умова істинна,
+		// то шукаємо максимум з трьох чисел
+		puts("define maximum of two numbers");
+		puts("Input 3 integer numbers");
+		puts("x : ");
+		scanf("%d", &x);// введення числа
+		PRINTI(x); // макрос виведення числа
+		puts("y : ");
+		scanf("%d", &y);// введення числа
+		PRINTI(y); // макрос виведення числа
+		puts("z : ");
+		PRINTI(z); // макрос виведення числа
+		max = MAX(x, z);
+		min = MIN(x, z);
+		value = min * max;  // розв'язуємо приклад
+		PRINTR(value);
+		modul = PLUS(value);
+		PRINTR(modul); // виводимо значення по модулю
+		a_kvadr = SQR(value); // макрос визначення квадрату числа
+		PRINTR(a_kvadr);
+		puts("Пiднести до степеня в рази - : ");
+		scanf("%d", &k);// введення числа
+		stepin = MULTI(value, k);
+		PRINTR(stepin);  // Виводим значення піднесене до степеня
+		puts("Пмаксимальне значення між ЧИСЛОМ(Т) і ЧИСЛОМ(Т) у квадраті  - : ");
+		maxim = MOREMAX(a_kvadr, value); // пошук максимуму з двох чисел
+		PRINTR(maxim); // макрос виведення результату*/
+#else // компілювати іншу частину програми, якщо N >= 10
+		{
+			puts("define maximum of two numbers");
+			puts("Input 3 integer numbers");
+			puts("x : ");
+			scanf("%d", x);// введення числа
+			PRINTI(x); // макрос виведення числа
+			puts("z : ");
+			PRINTI(z); // макрос виведення числа
+			value = SECOND(x, z);  // розв'язуємо приклад
+			PRINTR(value);
+			modul = PLUS(value);
+			puts("Module : ");
+			PRINTR(modul); // виводимо значення по модулю
+			puts("Корiнь числа : ");
+			a_kvadr = SQR(value); // макрос визначення квадрату числа
+			PRINTR(a_kvadr);
+			puts("Пiднести до степеня в : ");
+			scanf("%d", &k);// введення числа
+			stepin = MULTI(value, k);
+			PRINTR(stepin);
+		}
+#endif // завершення блоку умовної компіляції
+		puts("Repeat? y /n "); ch = getch();
+	} while (ch == 'y');
+}
---
+++ lab№11.1.cpp
@@ -0,0 +1,117 @@
+﻿//lab№11.1.cpp :
+//Сформувати файл даних, компонентами якого є числа.
+//Записати в інший файл даних середнє арифметичне максимальної і мінімальної
+//компоненти і середнє арифметичне всіх компонент.
+//Вивести на екран вміст результуючого файлу.
+
+#include <iostream>
+#include <fstream>
+#include <time.h>
+
+using namespace std;
+
+void CreateBIN(const char* fname) // створення файлу символів з введених рядків
+{
+	srand((unsigned)time(NULL));
+	int N, x;
+	cout << "Кiлькiсть значень у файлi: "; cin >> N;
+	ofstream f(fname, ios::binary); // відкрили файл для запису
+	for (int i = 0; i <= N - 1; i++)  // скануємо введений рядок
+	{
+		x = 0 + rand() % 21;
+		f.write((char*)&x, sizeof(x));  // записали символ у файл
+	}
+	cout << endl;
+}
+void PrintBIN(char* fname)
+{
+	ifstream fin(fname, ios::binary);
+	if (!fin)
+	{
+		cerr << "Error opening file '" << fname << "'" << endl;
+		return;
+	}
+	int c;
+	while (fin.read((char*)&c, sizeof(c)))
+	{
+		cout << c << " ";
+
+	}
+	cout << endl;
+	fin.close();
+}
+void ValueSER(char* fname, char* gname)
+{
+	ifstream f(fname, ios::binary);
+	ofstream h(gname, ios::binary);
+	if (!f)
+	{
+		cout << "Nothing" << endl;
+	}
+
+	float L = 0, S = 0;
+	int x, k = 0;
+
+	while (f.read((char*)&x, sizeof(x)))
+	{
+
+		k++;
+		S += x;
+	}
+	L = S / k;
+	cout << "\nL value : " << L;
+	cin.get();
+	cin.sync();
+	h.write((char*)&L, sizeof(L));
+	f.close();
+	h.close();
+}
+void ValueMaxMin(char* fname, char* gname)
+{
+	ifstream f(fname, ios::binary);
+	ofstream h(gname, ios::binary);
+
+	int L = 0, i = 0, max = 0, min = 0, x;
+
+	while (f.read((char*)&x, sizeof(x)))
+	{
+		if (i == 0)
+		{
+			min = x;
+			max = x;
+		}
+		if (min > x)
+		{
+			min = x;
+		}
+		if (max < x)
+		{
+			max = x;
+		}
+		i++;
+	}
+	L = (max + min) / 2.0;
+
+	h.write((char*)&L, sizeof(L));
+	f.close();
+	h.close();
+}
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	char fname[64]; // ім'я першого файлу
+	cout << "Введiть номер 1-го файлу : "; cin.getline(fname, sizeof(fname));
+	CreateBIN(fname); // ввели рядки файлу з клавіатури
+	PrintBIN(fname); // вивели вміст першого файлу на екран
+	cout << "\n_____________________________" << endl;
+
+	char gname[64]; // ім'я другого файлу
+	cout << "\nВведiть номер 2-го файлу : "; cin >> gname;
+	cout << "Среднє арифметичне значення  : " << endl;
+	ValueSER(fname, gname);
+	cout << "\n-------------" << endl;
+	cout << "Среднє значення мiж мiнiмальним i максимальним значенням : " << endl;
+	ValueMaxMin(fname, gname);
+	PrintBIN(gname); // вивели вміст другого файлу на екран
+	return 0;
+}
