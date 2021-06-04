964eb8a51d294bbeeb286bb6bfdbf16e22cf8e9b
Roman Koshiv <koshivr032@gmail.com>
2021-06-04 19:34:08 +0300

    Update №7.3 рекур.cpp

--- №7.3 рекур.cpp
+++ №7.3 рекур.cpp
@@ -1,11 +1,20 @@
 ﻿// Lab_7_3.cpp
 
-// < прізвище, ім’я автора >
 // Лабораторна робота No 7.3.
 // Опрацювання динамічних багатовимірних масивів. Рекурсивний спосіб.
 // Варіант 14
 
 //  Недокінчена
+
+//Здійснити циклічне переміщення квадратної матриці розміром(n  n) вправо на k
+//елементів таким чином : елементи першого рядка переміщаються в останній стовпець
+//зверху вниз, з нього – в останній рядок справа наліво, з нього – в перший стовпець від
+//низу до верху, з нього – в перший рядок; для решти елементів – аналогічно(оформитиу вигляді void - функції).
+
+//Здійснити циклічне переміщення квадратної матриці розміром(n  n) вправо на k
+//елементів таким чином : елементи першого рядка переміщаються в останній стовпець
+//зверху вниз, з нього – в останній рядок справа наліво, з нього – в перший стовпець від
+//низу до верху, з нього – в перший рядок; для решти елементів – аналогічно(оформитиу вигляді void - функції).
 
 #include <iostream>
 #include <iomanip>
@@ -19,174 +28,153 @@ void CreateRow(int** a, const int rowCount, const int colCount, const int Low, c
 //void InputRow(int** a, const int rowCount, const int colCount, int colNo);
 void PrintRows(int** a, const int rowCount, const int colCount, int rowNo);
 void PrintRow(int** a, const int rowCount, const int colCount, int rowNo, int colNo);
-
-void FirstRows(int** a, const int rowCount, const int colCount, int rowNo, int rowNo1, int colNo);
-void FirstRow(int** a, const int rowCount, const int colCount, int rowNo1, int rowNo, int colNo, int tmp);
-void Second(int** a, const int rowCount, const int colCount);
-void Third(int** a, const int rowCount, const int colCount);
-void Forth(int** a, const int rowCount, const int colCount);
+void FirstRows(int** a, const int rowCount, const int colCount, int colNo, int colNo1, int rowNo, int tmp);
+void FirstRow(int** a, const int rowCount, const int colCount, int colNo, int colNo1, int rowNo, int tmp);
+void SecondRows(int** a, const int rowCount, const int colCount, int rowNoleft, int colNoDown);
+void SecondRow(int** a, const int rowCount, const int colCount, int rowNoleft, const int colNoleft, const int rowNoDown, int colNoDown, int tmp);
+void ThirdRows(int** a, const int rowCount, const int colCount, int rowNoleft, int colNoDown);
+void ThirdRow(int** a, const int rowCount, const int colCount, int rowNoleft, const int colNoleft, const int rowNoDown, int colNoDown, int tmp);
+void ForthRows(int** a, const int rowCount, const int colCount, int rowUp, int colNoleft);
+void ForthRow(int** a, const int rowCount, const int colCount, int rowUp, int colUp, int colNoleft, int rowNoleft, int tmp);
 
 int main()
 {
 	srand((unsigned)time(NULL));
 	int Low = -17;
 	int High = 15;
-	int rowCount, colCount;
-	cout << "rowCount = "; cin >> rowCount;
-	cout << "colCount = "; cin >> colCount;
+	int valueCount, rowCount, colCount;
+	cout << "rowCount and colCount : "; cin >> valueCount;
+	rowCount = valueCount;
+	colCount = valueCount;
 
 	int** a = new int* [rowCount];
 	for (int i = 0; i < rowCount; i++)
 		a[i] = new int[colCount];
 
 	CreateRows(a, rowCount, colCount, Low, High, 0);
-	//InputRows(a, rowCount, colCount);
+	//InputRows(a, rowCount, colCount, 0);
 	PrintRows(a, rowCount, colCount, 0);
-	
+
 	cout << "Swap_First" << endl;
-	int colNo = colCount - 1;
-	int rowNo1 = colCount - 2;
-	FirstRows(a, rowCount, colCount, 1, rowNo1, colNo);
+	FirstRows(a, rowCount, colCount, colCount - 1, colCount - 2, 1, 0);
 	PrintRows(a, rowCount, colCount, 0);
 	cout << endl;
 
 	cout << "Swap_Second" << endl;
-	Second(a, rowCount, colCount);
+	SecondRows(a, rowCount, colCount, rowCount - 2, colCount - 2);
 	PrintRows(a, rowCount, colCount, 0);
-    cout << endl;
 
 	cout << "Swap_Third" << endl;
-	Third(a, rowCount, colCount);
+	ThirdRows(a, rowCount, colCount, 0, colCount - 1);
 	PrintRows(a, rowCount, colCount, 0);
 	cout << endl;
 
 	cout << "Swap_Forth" << endl;
-	Forth( a, rowCount, colCount);
+	ForthRows(a, rowCount, colCount, 0, 0);
 	PrintRows(a, rowCount, colCount, 0);
 	cout << endl;
 
-	for (int i = 0; i < rowCount; i++)
-		delete[] a[i];
-	delete[] a;
+	cout << endl;
 	return 0;
 }
-void CreateRow(int** a, const int rowCount, const int colCount, const int Low, const int High, int rowNo, int colNo)
-{
-	a[rowNo][colNo] = Low + rand() % (High - Low + 1);
-	if (colNo < colCount)
-		CreateRow(a, rowCount, colCount, Low, High, rowNo, colNo + 1);
-}
-void CreateRows(int** a, const int rowCount, const int colCount, const int Low, const int High, int rowNo)
-{
-	CreateRow(a, rowCount, colCount, Low, High, rowNo, 0);
-		if (rowNo < rowCount - 1)
-			CreateRows(a, rowCount, colCount, Low, High, rowNo + 1);
-}
-void InputRow(int** a, const int rowCount, const int colCount, int rowNo, int colNo)
+void InputRow(int** a, const int rowNo, const int colCount, int colNo)
 {
 	cout << "a[" << rowNo << "][" << colNo << "] = ";
 	cin >> a[rowNo][colNo];
 	if (colNo < colCount - 1)
-		InputRow(a, rowCount, colCount, rowNo, colNo + 1);
+		InputRow(a, rowNo, colCount, colNo + 1);
 	else
 		cout << endl;
 }
 void InputRows(int** a, const int rowCount, const int colCount, int rowNo)
 {
-	InputRow(a, rowCount, colCount, rowNo, 0);
+	InputRow(a, rowNo, colCount, 0);
 	if (rowNo < rowCount - 1)
-		InputRows(a, rowCount, colCount, rowNo);
+		InputRows(a, rowCount, colCount, rowNo + 1);
 	else
 		cout << endl;
 }
-void PrintRow(int** a, const int rowCount, const int colCount, int rowNo, int colNo)
+void CreateRow(int** a, const int rowNo, const int colCount, const int Low, const int High, int colNo)
 {
-	cout << a[rowNo][colNo];
-	    cout << endl;
+	a[rowNo][colNo] = Low + rand() % (High - Low + 1);
 	if (colNo < colCount - 1)
-	PrintRow(a, rowCount, colCount, rowNo, colNo + 1);
+		CreateRow(a, rowNo, colCount, Low, High, colNo + 1);
+}
+void CreateRows(int** a, const int rowCount, const int colCount, const int Low, const int High, int rowNo)
+{
+	CreateRow(a, rowNo, colCount, Low, High, 0);
+	if (rowNo < rowCount - 1)
+		CreateRows(a, rowCount, colCount, Low, High, rowNo + 1);
+}
+void PrintRow(int** a, const int rowNo, const int colCount, int colNo)
+{
+	cout << setw(4) << a[rowNo][colNo];
+	if (colNo < colCount - 1)
+		PrintRow(a, rowNo, colCount, colNo + 1);
 	else
 		cout << endl;
 }
 void PrintRows(int** a, const int rowCount, const int colCount, int rowNo)
 {
-	PrintRow(a, rowCount, colCount, rowNo, 0);
+	PrintRow(a, rowNo, colCount, 0);
 	if (rowNo < rowCount - 1)
 		PrintRows(a, rowCount, colCount, rowNo + 1);
 	else
 		cout << endl;
 }
-void FirstRow(int** a, const int rowCount, const int colCount, int rowNo1, int rowNo, int colNo, int tmp)
+void FirstRows(int** a, const int rowCount, const int colCount, int colNo, int colNo1, int rowNo, int tmp)
 {
-		tmp = a[0][rowNo1];
-		a[0][rowNo1] = a[rowNo][colNo];
-		a[rowNo][colNo] = tmp;
-		FirstRows(a, rowCount, colCount, rowNo + 1, rowNo1, colNo);
-}
-void FirstRows(int** a, const int rowCount, const int colCount, int rowNo, int rowNo1, int colNo)
-{
-	if (rowNo < rowCount)
-	FirstRow(a, rowCount, colCount, rowNo1, rowNo, colNo, 0);
-	if (rowNo1 != -1)
-		FirstRows(a, rowCount, colCount, rowNo, rowNo1 - 1, colNo);
-	else
-		cout << endl;
-}
-void Second(int** a, const int rowCount, const int colCount)
-{
-	int tmp;
-	int i = rowCount - 1;
-	const int j = colCount - 1;
-	int rowNo = rowCount - 1;
-	int colNo = colCount - 1;
-	while (i != -1 && colNo != -1)
+	if (colNo1 != -1 && rowNo < rowCount)
 	{
-		tmp = a[i][j];
-		a[i][j] = a[rowNo][colNo];
-		a[rowNo][colNo] = tmp;
-		i--;
-		colNo--;
+		FirstRow(a, rowCount, colCount, colNo, colNo1, rowNo, 0);
 	}
 }
-void Third(int** a, const int rowCount, const int colCount)
+void FirstRow(int** a, const int rowCount, const int colCount, int colNo, int colNo1, int rowNo, int tmp)
 {
-	int tmp;
-	int i = 0;
-	int j = 0;
-	int rowNo = rowCount - 1;
-	int colNo = colCount - 1;
-	while (i < rowCount - 1 && colNo != -1)
+	tmp = a[0][colNo1];
+	a[0][colNo1] = a[rowNo][colNo];
+	a[rowNo][colNo] = tmp;
+	FirstRows(a, rowCount, colCount, colNo, colNo1 - 1, rowNo + 1, tmp);
+}
+void SecondRows(int** a, const int rowCount, const int colCount, int rowNoleft, int colNoDown)
+{
+	if (rowNoleft != -1 && colNoDown != -1)
 	{
-		a[i][j];
-		a[rowNo][colNo];
-
-		tmp = a[i][j];
-		a[i][j] = a[rowNo][colNo];
-		a[rowNo][colNo] = tmp;
-		i++;
-		colNo--;
+		SecondRow(a, rowCount, colCount, rowNoleft, colCount - 1, rowCount - 1, colNoDown, 0);
 	}
 }
-void Forth(int** a, const int rowCount, const int colCount)
+void SecondRow(int** a, const int rowCount, const int colCount, int rowNoleft, const int colNoleft, const int rowNoDown, int colNoDown, int tmp)
 {
-	int tmp;
-	int i = 1;
-	int j = 0;
-	int rowNo = 0;
-	int colNo = 1;
-	while (i < rowCount && colNo < colCount)
+	tmp = a[rowNoleft][colNoleft];
+	a[rowNoleft][colNoleft] = a[rowNoDown][colNoDown];
+	a[rowNoDown][colNoDown] = tmp;
+	SecondRows(a, rowCount, colCount, rowNoleft - 1, colNoDown - 1);
+}
+void ThirdRows(int** a, const int rowCount, const int colCount, int rowNoleft, int colNoDown)
+{
+	if (rowNoleft < rowCount - 1 && colNoDown != 0)
 	{
-		a[i][j];
-		a[rowNo][colNo];
-
-		tmp = a[i][j];
-		a[i][j] = a[rowNo][colNo];
-		a[rowNo][colNo] = tmp;
-		i++;
-		colNo++;
+		ThirdRow(a, rowCount, colCount, rowNoleft, 0, rowCount - 1, colNoDown, 0);
 	}
 }
-//Здійснити циклічне переміщення квадратної матриці розміром(n  n) вправо на k
-//елементів таким чином : елементи першого рядка переміщаються в останній стовпець
-//зверху вниз, з нього – в останній рядок справа наліво, з нього – в перший стовпець від
-//низу до верху, з нього – в перший рядок; для решти елементів – аналогічно(оформитиу вигляді void - функції).*/
+void ThirdRow(int** a, const int rowCount, const int colCount, int rowNoleft, const int colNoleft, const int rowNoDown, int colNoDown, int tmp)
+{
+	tmp = a[rowNoleft][colNoleft];
+	a[rowNoleft][colNoleft] = a[rowNoDown][colNoDown];
+	a[rowNoDown][colNoDown] = tmp;
+	ThirdRows(a, rowCount, colCount, rowNoleft + 1, colNoDown - 1);
+}
+void ForthRows(int** a, const int rowCount, const int colCount, int rowUp, int colNoleft)
+{
+	if (rowUp < rowCount && colNoleft < colCount)
+	{
+		ForthRow(a, rowCount, colCount, rowUp, 0, colNoleft, 0, 0);
+	}
+}
+void ForthRow(int** a, const int rowCount, const int colCount, int rowUp, int colUp, int colNoleft, int rowNoleft, int tmp)
+{
+	tmp = a[rowUp][colUp];
+	a[rowUp][colUp] = a[rowNoleft][colNoleft];
+	a[rowNoleft][colNoleft] = tmp;
+	ForthRows(a, rowCount, colCount, rowUp + 1, colNoleft + 1);
+}
