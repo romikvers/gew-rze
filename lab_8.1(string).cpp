f6fc70465b41af21e1d68b47f3a601881b17e940
Roman Koshiv <koshivr032@gmail.com>
2021-06-14 11:08:19 +0300

    Revert "Update lab_8.1(string).cpp"
    This reverts commit 9fc01ed1d2e1854c2d9aa95bff5af320634e2b3f.

--- lab_8.1(string).cpp
+++ lab_8.1(string).cpp
@@ -1,37 +1,50 @@
 ﻿//Дано літерний рядок, який містить послідовність символів s0, ..., sn, ... .Відомо, що
 //серед цих символів є по крайній мірі три крапки.
+//1.  Знайти число і таке, що sі – третя за порядком крапка.
 //2.  Замінити кожний четвертий символ крапкою.
-
 
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-string Change(string& s)
+int Count(const string s)
 {
-	string S = s;
-	size_t N = S.length();
-	if (3 >= N)
+	int k = 0;
+	size_t pos = 0;
+	while((pos = s.find('.', pos)) != -1)
 	{
-		cout << "Mas ist klein" << endl;
-		return S;
+		pos++;
+		k++;
+		if (k == 3)
+			return pos;
 	}
-	size_t pos = 3;
-	while(pos < N)
+	cout << "Nothing\n";
+	return -1;
+}
+string Change(string& s) // не працює
+{
+	int N = s.length();
+	if (3 < N)
 	{
-		S.replace(pos, 1, ".");
-		pos += 4;
+		for (size_t pos = 3; pos < N - 4; pos + 4)
+		{
+			s.replace(pos, 1, "..");
+		}
+		return s;
 	}
-	return S;
+	cout << "Mas ist klein";
+return 0;	
 }
 
 int main()
 {
-	string str = "today is good weather ... ja genau!";
+	setlocale(LC_ALL, "rus");
+	string str;
 
-	//cout << "Enter line\n";
-	//getline(cin, str);
+	cout << "Enter line\n";
+	getline(cin, str);
+	cout << "\n Lenght punkt\".\"\n" << Count(str) << endl;
 
 	string dest = Change(str);
 	cout << "Modified string (param) : " << str << endl;
