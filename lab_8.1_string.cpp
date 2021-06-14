6e6b3f7968e381bf6921645c6526afda3ca92282
Roman Koshiv <koshivr032@gmail.com>
2021-06-14 11:14:06 +0300

    Create lab_8.1_string.cpp

---
+++ lab_8.1_string.cpp
@@ -0,0 +1,41 @@
+﻿//Дано літерний рядок, який містить послідовність символів s0, ..., sn, ... .Відомо, що
+//серед цих символів є по крайній мірі три крапки.
+//2.  Замінити кожний четвертий символ крапкою.
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string Change(string& s)
+{
+	string S = s;
+	size_t N = S.length();
+	if (3 >= N)
+	{
+		cout << "Mas ist klein" << endl;
+		return S;
+	}
+	size_t pos = 3;
+	while(pos < N)
+	{
+		S.replace(pos, 1, ".");
+		pos += 4;
+	}
+	return S;
+}
+
+int main()
+{
+	string str = "today is good weather ... ja genau!";
+
+	//cout << "Enter line\n";
+	//getline(cin, str);
+
+	string dest = Change(str);
+	cout << "Modified string (param) : " << str << endl;
+	cout << "Modified string (result): " << dest << endl;
+	
+	return 0;
+}
