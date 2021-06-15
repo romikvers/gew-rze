aeaafbfaa1d137023dd6f711dada908884a4a220
Roman Koshiv <koshivr032@gmail.com>
2021-06-15 10:15:04 +0300

    Update №8.1 рекурсивний.cpp

--- №8.1 рекурсивний.cpp
+++ №8.1 рекурсивний.cpp
@@ -1,44 +1,51 @@
 ﻿//Дано літерний рядок, який містить послідовність символів s0, ..., sn, ... .Відомо, що
 //серед цих символів є по крайній мірі три крапки.
-//1.  Знайти число і таке, що sі – третя за порядком крапка.
 //2.  Замінити кожний четвертий символ крапкою.
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <locale>
 #include <string>
+
 using namespace std;
 
-int Street(char* str, int i, int n)
+char* Change(char* dest, const char* str, char* t, int m, int i)
 {
-	if (strlen(str) < 2)
-		return 0;
 	if (str[i] != 0)
 	{
-		if (str[i] == '.')
+		if (i == m)
 		{
-			if (n == 3)
-			{
-				return i + 1;
-			}
-			else
-				return Street(str, i + 1, n + 1);
+			strcat(t, ".");
+			return Change(dest, str, t + 1, m + 4, i + 1);
 		}
 		else
-			return Street(str, i + 1, n);
+		{
+			*t++ = str[i++];
+			*t = '\0';
+			return Change(dest, str, t, m, i);
+		}
 	}
 	else
-		return 0;
+	{
+		*t++ = str[i++];
+		*t++ = str[i++];
+		*t = '\0';
+		return dest;
+	}
 }
-
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	char str[101];
-
-	cout << "Enter string:" << endl;
-	cin.getline(str, 100);
-	cout << "String contained " << Street(str, 0, 1) << " groups of later" << endl;
+	//char str[101];
+	char str[] = "today is good weather";
+	//cout << "Enter string:" << endl;
+	//cin.getline(str, 150);
+	cout << "Лiтерний рядок до обробки : " << str << endl;
+	char* dest1 = new char[151];
+	dest1[0] = '\0';
+	char* dest2;
+	dest2 = Change(dest1, str, dest1, 3, 0);
+	cout << "Лiтерний рядок пiсля обробки (result) : " << dest2 << endl;
 
 	return 0;
-} 
+}
