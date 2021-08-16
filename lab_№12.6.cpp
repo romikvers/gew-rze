bbb89810c67894fa9de68d2ade5a938cd3b62133
romikvers <koshivr032@gmail.com>
2021-08-16 19:44:52 +0300

    Update lab_№12.6.cpp

--- lab_№12.6.cpp
+++ lab_№12.6.cpp
@@ -1,11 +1,5 @@
 ﻿// lab_№12.6.cpp : 
 
-/*Необхідно:
--сформувати заданий список(списки);
--роздрукувати його(їх) – вивести значення елементів на екран;
--виконати вказані в завданні дії;
--вивести результат на екран.
-*/
 //  Промоделювати процес посадки в літак, що розрахований на 100 місць. В черзі на 
 // посадку стоять 100 пасажирів.Першою входить старий чоловік.Зайшовши в салон, він сідає
 // на будь - яке випадково вибране місце.Кожний з наступних пасажирів, зайшовши в салон,
@@ -14,6 +8,7 @@
 
 #include <iostream>
 #include <time.h>
+#include <vector>
 #include <Windows.h>
 
 using namespace std;
@@ -28,12 +23,12 @@ struct Elem
 };
 
 void CreatTicket(int* Stuhl);
-int CompareTicket(int* Repeat, int buy);
 void PrintTicket(int* Stuhl);
 void enqueue(Elem*& first, Elem*& last, Info value);
 void Print(Elem* L);
 Info dequeue(Elem*& first, Elem*& last);
-int Rechnung(Elem*& first, Elem*& last, int* Stuhl, int* busy);
+int Check(const int placepassenger, int* busy, const int size, bool on_off);
+int Rechnung(int* Stuhl, int* busy);
 
 int main()
 {
@@ -48,78 +43,103 @@ int main()
 
     int Stuhl[100]; 
     int busy[100];      //    зайняті місця
-    cout << "Білет кожного пасажира :\n";
     CreatTicket(Stuhl);
     PrintTicket(Stuhl);
-    int k = Rechnung(first, last, Stuhl, busy);
-    if(k == 1)
-    {
-       cout << "Пасажир сів на місце, яке є вказане в його білеті" << endl;
-    }
-    else 
-       cout << "Пасажир не сів на місце, яке є вказане в його білеті" << endl;
-    Info m; // Особа яка сідає на місце під номером ...
-    for (int k = 0; k < 100; k++)     //  порядок заповнення пасажирами літак
+    int k = Rechnung(Stuhl, busy);
+
+    int size = sizeof(busy) / sizeof(*busy);   // розмір масиву
+    Info m = 0; // Особа яка сідає на місце під номером ...
+    for (int k = 0; k < size; k++)     //  порядок заповнення пасажирами літак
     {
         m = busy[k];
         enqueue(first, last, m);
     }
+    cout << "\nМісця на які сіли пасажири від 1-100 :";
     // Print(first);
-    cout << "" << dequeue(first, last);
-
+    Info value = 0;
+    for (int i = 0; i < size; i++)
+    {
+        value = dequeue(first, last);
+        cout << value << " ";
+    }
+    if(k == 1 || value == busy[size - 1])
+    {
+       cout << endl;
+       cout << "Пасажир сів на місце, яке є вказане в його білеті" << endl;
+       cout << endl;
+    }
+    else {
+        cout << endl;
+        cout << "Пасажир не сів на місце, яке є вказане в його білеті" << endl;
+        cout << endl;
+    }
     return 0;
 }
 void CreatTicket(int* Stuhl)
 {
-    int buy, k;
-    int Repeat[100];
-    for (int i = 0; i < 100; i++)
+    int buy = 0,    // номер купленого квитка дідом 
+        index_buy = 0,
+        num_ticket = 0;
+    vector<int> ticket{};    //   загружаємо у вектор номера квитків
+    for (int num_ticket = 1; num_ticket <= 100; num_ticket++)  //  заповнюємо вектор номерами квитків
     {
-        do {
-            buy = 1 + rand() % 100;
+        ticket.push_back(num_ticket);
+    }
+    for (int i = 0; i < 100; i++)   // (індекс це особа, значення - це квиток...
+    {
+        int size_vec = ticket.size();
+        
+        index_buy = 1 + rand() % size_vec;    // квиток особи
 
-            k = CompareTicket(Repeat, buy);
-        } while (k == 0);
+        buy = ticket[index_buy - 1];   // починаються адреси мисивів з 0
 
-        Stuhl[i] = buy;
+        Stuhl[i] = buy;     //  присвоємо особі з індексом [ i ], значення buy
 
-        Repeat[i] = buy;
+        ticket.erase(ticket.begin() + index_buy - 1);    // видаляємо елемент з вектору, який вже записаний в масив Stuhl[]
     }
 }
-int CompareTicket(int* Repeat, int buy)
+void PrintTicket(int* Stuhl)   // зчитуємо 
 {
-    for (int l = 0; l < 100; l++)
-    {
-        if (Repeat[l] == buy) {
-            return 0;
-        }
-    }
-    return 1;
-}
-void PrintTicket(int* Stuhl)
-{
-    int k = 1, i = 0;
-    cout << " [" << k << "] - " << Stuhl[i];
+    int k = 1,    //  пасажир №...
+        i = 0;    //  квиток з номером крісла...
+    cout << "Пасажири з квитками:\n [" << k << "] - " << Stuhl[i];
     for (i = 1; i < 100; i++)
     {
         k = i;
         cout << ", [" << k + 1 << "] - " << Stuhl[i];
     }
 }
-int Rechnung(Elem*& first, Elem*& last, int* Stuhl, int* busy)
+int Check(const int placepassenger, int* busy, const int size, bool on_off)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (busy[i] == NULL) {
+            break;
+        }
+        if (placepassenger == busy[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+int Rechnung(int* Stuhl, int* busy)
 {
     int Mann;    // Номер чоловіка який заходить
-    Mann = 0;    // перша особа яка заходить (дід)
+    Mann = 0;    // особа яка заходить (дід),  0 - індекс першої особи - ДІДА
 
     int biletnum = Stuhl[Mann];         // білет діда на якому є номер крісла на яке він має сісти  
 
-    int placeOpa = 1 + rand() % 100;    //   місце, за яке сідає дідо 
+    int placeOpa = 1 + rand() % 100;    //   місце, на яке сідає дідо 
 
-    busy[Mann] = placeOpa;           // місця на яких вже сидять пасажири
+    busy[Mann] = placeOpa;           // місця на якому сидить дід, записали як зайняте
 
-    int placepassenger; // Особа займає місце під номером...
+    int placepassenger = 0; // Особа займає місце під номером...
+
+    int size = sizeof(busy) / sizeof(*busy);   //  визначаємо загальний розмір масива
+
     if (biletnum == placeOpa)    // Дідо сів на зазначене в білеті місце
     {
+        Mann++;   //   тому що, діда вже записали
         while (Stuhl[Mann] != NULL) {
             placepassenger = Stuhl[Mann];
 
@@ -131,33 +151,42 @@ int Rechnung(Elem*& first, Elem*& last, int* Stuhl, int* busy)
     }
     if (biletnum != placeOpa)       //   дідо не сів на місце під номером biletnum
     {
+        int switch_on_off = 0,    // вказує -1- коли в busy[...] є це місця
+        busynumm = 0;     //   місце під номером
         do
         {
             Mann++;
-            int busynumm;     //   місце під номером
-            do {
-                for (busynumm = 0; busynumm < 100; busynumm++)
+            switch_on_off = 0;
+
+                for (int i = 0; i < 100; i++)
                 {
-                    if (Stuhl[Mann] == busy[busynumm])
+                    if (busy[i] == NULL) {
                         break;
+                    }
+                    if (Stuhl[Mann] == busy[i]) {
+                        switch_on_off = 1;
+                        break;
+                    }
                 }
-                if (Stuhl[Mann] != busy[busynumm])
+                if (switch_on_off == 0)
                 {
                     placepassenger = Stuhl[Mann];
                 }
                 else
-                    if (Stuhl[Mann] == busy[busynumm])
+                    if (switch_on_off == 1)
                     {
-                        placepassenger = 1 + rand() % 100;
+                        bool chek = true,
+                        on_off = true;    // перевіряє чи зайняте задане рандомно місце, true - не зайняте. 
+                        do {
+                            placepassenger = 1 + rand() % 100;
+                        } while ((chek = Check(placepassenger, busy, size, on_off)) == false);
                     }
-            } while (placepassenger != Stuhl[Mann]);
 
-            busy[busynumm++] = placepassenger;
-
+             busynumm++;
+             busy[busynumm] = placepassenger;
         } while (Stuhl[Mann] != NULL);
-       // cout << "Пасажир не сів на місце, яке є вказане в його білеті" << endl;
-        return 0;
     }
+    return 0;
 }
 void enqueue(Elem*& first, Elem*& last, Info value)
 {
